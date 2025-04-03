03f8f92f947befa63d9be82d987ae9e9fa7df935
Praveen-0408 <praveeneslavathkumar@gmail.com>
2025-04-03 10:02:14 +0530

    Updated project

--- src/app/dashboard/tradeexecution/page.js
+++ src/app/dashboard/tradeexecution/page.js
@@ -3,7 +3,7 @@
     import { db } from "@/firebase/config";
     import { doc, getDoc, updateDoc, collection, addDoc, serverTimestamp } from "firebase/firestore";
     import { useSearchParams, useRouter } from "next/navigation"; 
-    import { auth } from "@/firebase/auth"; // Firebase Authentication
+    import { auth } from "@/firebase/config"; // Firebase Authentication
     import { commodityData } from "@/utils/commodityData";
 
 
