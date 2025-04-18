4f50067be92c7502410c5efc275a9aed6238af94
Praveen-0408 <praveeneslavathkumar@gmail.com>
2025-04-18 06:05:11 +0530

    Updated page.js in the deposit dashboard

--- src/app/dashboard/deposit/page.js
+++ src/app/dashboard/deposit/page.js
@@ -58,7 +58,7 @@ export default function Deposit() {
     <div className={styles.container}>
       <h2 className={styles.title}>Deposit Funds</h2>
       <p>Scan the QR Code to make payment</p>
-      <Image src="/example.jpg" alt="Example Image" width={500} height={300} />
+      <Image src="/qrcode.svg" alt="Image" width={400} height={200} priority />
 
       <input
         type="number"
